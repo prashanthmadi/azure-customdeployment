2fb678174e42a46615a8a534808ee459bf34574e
ramyagangula <ramya1331@gmail.com>
2016-04-26 22:37:29 -0500

    Signed-off-by: ramyagangula <ramya1331@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+// minify new images
+gulp.task('imagemin', function() {
+  var imgSrc = './src/images/**/*',
+      imgDst = './build/images';
+
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
